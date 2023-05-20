10bf0dde73571116e1fed8993e001d6ad6b54f85
diamond9159 <johnford9159@gmail.com>
2023-05-20 13:58:38 -0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,77 @@
+import React, { useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const [HDSerialNumber, setHDSerialNumber] = useState('0000000000000');
+
+  const getMachineId = () => {
+    let machineId = localStorage.getItem('MachineId');
+
+    if (!machineId) {
+      machineId = crypto.randomUUID();
+      localStorage.setItem('MachineId', machineId);
+    }
+
+    return machineId;
+  };
+
+  const getComputerInfo = () => {
+    var locator = new ActiveXObject('WbemScripting.SWbemLocator');
+    var service = locator.ConnectServer('.');
+    var properties = service.ExecQuery('SELECT * FROM Win32_BaseBoard');
+    var e = new Enumerator(properties);
+
+    const computerInfo = {};
+    document.write('<table border=1>');
+    for (; !e.atEnd(); e.moveNext()) {
+      var p = e.item();
+
+      console.log(p);
+
+      document.write('<tr>');
+      document.write('<td>' + p.HostingBoard + '</td>');
+      document.write('<td>' + p.Manufacturer + '</td>');
+      document.write('<td>' + p.PoweredOn + '</td>');
+      document.write('<td>' + p.Product + '</td>');
+      document.write('<td>' + p.SerialNumber + '</td>');
+      document.write('<td>' + p.Version + '</td>');
+      document.write('</tr>');
+    }
+    document.write('</table>');
+  };
+
+  const getHardDriveSerialNumber = () => {
+    return new Promise(function (resolve, reject) {
+      const wmi = GetObject('winmgmts:{impersonationLevel=impersonate}');
+      const c = new Enumerator(
+        wmi.ExecQuery('SELECT SerialNumber FROM Win32_DiskDrive')
+      );
+      for (; !c.atEnd(); c.moveNext()) {
+        const drive = c.item();
+        const serial = drive.SerialNumber;
+        resolve(serial);
+      }
+      reject();
+    });
+  };
+
+  const GetMachineInfo = () => {
+    getHardDriveSerialNumber()
+      .then((serial) => {
+        console.log('Hard Drive Serial Number: ' + serial);
+        setHDSerialNumber(serial);
+      })
+      .catch(() => {
+        console.log('Error getting hard drive serial number.');
+      });
+
+    const machineId = getMachineId();
+    setHDSerialNumber(machineId);
+    
+    getComputerInfo();
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,15 +83,12 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
           I would like to get Hard drive serial number.
-          <button className={styles.card}>Get Serial Number</button>
-          <code>gdfsgergre</code>
+          <button className={styles.card} onClick={() => GetMachineInfo()}>
+            Get Serial Number
+          </button>
+          <code>{HDSerialNumber}</code>
         </div>
       </main>
 
